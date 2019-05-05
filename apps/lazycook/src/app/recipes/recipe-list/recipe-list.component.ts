a08ab45c6699eb45f8895fa30095b1e1349d7bc5
Zilong Li <zilong.li@deliveryhero.com>
2019-05-05 18:03:21 +0200

    recipe list & detail layout

--- apps/lazycook/src/app/recipes/recipe-list/recipe-list.component.ts
+++ apps/lazycook/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,13 +1,19 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Recipe } from '../recipe.model';
+
 @Component({
   selector: 'lc-recipe-list',
   templateUrl: './recipe-list.component.html',
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe('Pizza', 'Kebab pizza.', 'http://bit.do/eRzsH'),
+  ];
 
-  constructor() { }
+  constructor() {
+  }
 
   ngOnInit() {
   }
